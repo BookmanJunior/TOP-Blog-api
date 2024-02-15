c750d598a0ed8e869b0f20dcfcb2657e9b35fb23
Azam Gulov <gulovazam@gmail.com>
2024-02-15 20:08:48 +0500

    Add function to connect to db
    add article route
    configure cors

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+main().catch(() => console.log("Failed to connect to DB"));
+
+async function main() {
+  mongoose.connect(process.env.DBURI);
+}
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const articleRouter = require("./routes/article");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/article", articleRouter);
 
 module.exports = app;
