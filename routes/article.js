14cdd67bb268e89d9e82fd3448e4c5de945849f1
Azam Gulov <gulovazam@gmail.com>
2024-02-15 20:12:57 +0500

    Set up article routes

---
+++ routes/article.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const articleController = require("../controllers/articleController");
+
+router.get("/", articleController.articles_get);
+
+router.get("/:id", articleController.article_get);
+
+router.post("/new", articleController.article_post);
+
+module.exports = router;
