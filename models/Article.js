6ed0ceed5d86d0077d0847595089632dfd3cbf28
Azam Gulov <gulovazam@gmail.com>
2024-02-16 20:07:03 +0500

    Update article schema

--- models/Article.js
+++ models/Article.js
@@ -3,9 +3,12 @@ const { Schema, model } = require("mongoose");
 const ArticleSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  headerImage: { type: String },
-  author: { type: Schema.Types.ObjectId, ref: "User" },
+  cover: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   featured: { type: Boolean, default: false },
+  published: { type: Boolean, default: true },
+  date: { type: Date, default: Date.now() },
 });
 
 module.exports = model("Article", ArticleSchema);
