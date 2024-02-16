846b285a198628ce4f97bc999abf8ac441c3c7b8
Azam Gulov <gulovazam@gmail.com>
2024-02-16 20:49:39 +0500

    Add article edit middleware

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -66,3 +66,46 @@ exports.article_post = [
     }
   },
 ];
+
+exports.article_edit = [
+  body("title", "Title can't be blank").trim().isLength({ min: 1 }),
+  body("content", "Article content can't be blank").trim().isLength({ min: 1 }),
+  body("cover", "Article cover can't be empty")
+    .trim()
+    .custom(async (value) => {
+      try {
+        new URL(value);
+      } catch (error) {
+        throw new Error("Invalid Url. Try again.");
+      }
+    })
+    .escape(),
+  body("author", "Author can't be empty").trim().isLength({ min: 1 }).escape(),
+  body("featured").trim().optional({ checkFalsy: true }).isBoolean().escape(),
+  body("published").trim().optional({ checkFalsy: true }).isBoolean().escape(),
+
+  async function (req, res, next) {
+    const errors = validationResult(req);
+    const articleId = req.params.id;
+    const article = new Article({
+      title: req.body.title,
+      content: req.body.content,
+      cover: req.body.cover,
+      author: req.body.author,
+      featured: req.body.featured,
+      published: req.body.published,
+      _id: articleId,
+    });
+
+    if (!errors.isEmpty()) {
+      return res.status(422).send(errors);
+    }
+
+    try {
+      await Article.findByIdAndUpdate(articleId, article);
+      return res.status(200).send(article);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+];
