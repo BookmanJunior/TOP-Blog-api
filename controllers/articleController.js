bf3b3fa2a9b74d0776dd3c25df51b1e3140da521
Azam Gulov <gulovazam@gmail.com>
2024-02-16 20:08:33 +0500

    Update article post middleware
    add custom cover validator

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -29,23 +29,40 @@ exports.article_get = async function (req, res, next) {
 exports.article_post = [
   body("title", "Title can't be blank").trim().isLength({ min: 1 }),
   body("content", "Article content can't be blank").trim().isLength({ min: 1 }),
+  body("cover", "Article cover can't be empty")
+    .trim()
+    .custom(async (value) => {
+      try {
+        new URL(value);
+      } catch (error) {
+        throw new Error("Invalid Url. Try again.");
+      }
+    })
+    .escape(),
+  body("author", "Author can't be empty").trim().isLength({ min: 1 }).escape(),
+  body("featured").trim().optional({ checkFalsy: true }).isBoolean().escape(),
+  body("published").trim().optional({ checkFalsy: true }).isBoolean().escape(),
 
   async function (req, res, next) {
     const errors = validationResult(req);
     const article = new Article({
       title: req.body.title,
       content: req.body.content,
+      cover: req.body.cover,
+      author: req.body.author,
+      featured: req.body.featured,
+      published: req.body.published,
     });
 
     if (!errors.isEmpty()) {
-      return res.status(422).send({ errors, article });
+      return res.status(422).send(errors);
     }
 
     try {
       await article.save();
       return res.status(200).send(article);
     } catch (error) {
-      return next(error);
+      return res.status(400).send(error);
     }
   },
 ];
