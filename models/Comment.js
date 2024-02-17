bd434a225ea81167549870868ba0741caaccfe24
Azam Gulov <gulovazam@gmail.com>
2024-02-17 22:30:46 +0500

    Bug fix

--- models/Comment.js
+++ models/Comment.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const CommentSchema = new Schema({
-  text: { type: string, required: true },
+  text: { type: String, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   article: { type: Schema.Types.ObjectId, ref: "Article", required: true },
   date: { type: Date, default: Date.now() },
