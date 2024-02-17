515387764c9196298e3b6380c50a046466b294b1
Azam Gulov <gulovazam@gmail.com>
2024-02-17 19:06:21 +0500

    Add delete route
    change put route

--- routes/article.js
+++ routes/article.js
@@ -4,10 +4,12 @@ const articleController = require("../controllers/articleController");
 
 router.get("/", articleController.articles_get);
 
+router.post("/", articleController.article_post);
+
 router.get("/:id", articleController.article_get);
 
-router.post("/new", articleController.article_post);
+router.put("/:id", articleController.article_edit);
 
-router.put("/:id/edit", articleController.article_edit);
+router.delete("/:id", articleController.article_delete);
 
 module.exports = router;
