60b11ce4d9ba9f6c5e14b9ff0df4d6acababbf02
Azam Gulov <gulovazam@gmail.com>
2024-02-17 19:17:50 +0500

    Add date field

--- models/Comment.js
+++ models/Comment.js
@@ -4,6 +4,7 @@ const CommentSchema = new Schema({
   text: { type: string, required: true },
   user: { type: Schema.Types.ObjectId, ref: "User", required: true },
   article: { type: Schema.Types.ObjectId, ref: "Article", required: true },
+  date: { type: Date, default: Date.now() },
 });
 
 module.exports = model("Comment", CommentSchema);
