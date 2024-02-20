867934e222712f77ad1504f74b1ca8f7bd2ddcb7
Azam Gulov <gulovazam@gmail.com>
2024-02-20 22:33:36 +0500

    Rename file
    fix LocalStrategy import

---
+++ Auth/Authentication.js
@@ -0,0 +1,25 @@
+const LocalStrategy = require("passport-local").Strategy;
+const User = require("../models/User");
+
+const errorMessage = "Wrong username or password";
+
+const strategy = new LocalStrategy(async (username, password, done) => {
+  try {
+    const user = await User.findOne({ username }).exec();
+    if (!user) {
+      return done(null, false, { message: errorMessage });
+    }
+
+    const isCorrectPassword = await user.validatePassword(password);
+    console.log(isCorrectPassword);
+    if (!isCorrectPassword) {
+      return done(null, false, { message: errorMessage });
+    }
+
+    return done(null, user);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = strategy;
