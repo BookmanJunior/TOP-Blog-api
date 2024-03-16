278128435ea379df3daa4356e8925223d725162f
Azam Gulov <gulovazam@gmail.com>
2024-03-16 20:28:53 +0500

    Add login_cms controller

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -13,3 +13,20 @@ exports.login = (req, res, next) => {
     });
   })(req, res, next);
 };
+
+exports.login_cms = (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return next(err);
+
+    if (!user) return res.status(403).send({ credentials: info.message });
+
+    if (user.role !== "admin")
+      return res.status(403).send({ error: "Unauthorized" });
+
+    req.logIn(user, (err) => {
+      if (err) return next(err);
+
+      return res.status(200).send({ username: user.username, role: user.role });
+    });
+  })(req, res, next);
+};
