bdb66209be9e8d3b743f733e6aae2f833f0288d3
Azam Gulov <gulovazam@gmail.com>
2024-03-16 20:36:01 +0500

    Make post request a protected route
    remove put and delete routes

--- routes/article.js
+++ routes/article.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/articleController");
+const authorizationController = require("../controllers/authorizationController");
 
 router.get("/", articleController.articles_get);
 
-router.post("/", articleController.article_post);
+router.post(
+  "/",
+  authorizationController.isAdmin,
+  articleController.article_post
+);
 
 router.get("/:id", articleController.article_get);
-
-router.put("/:id", articleController.article_edit);
-
-router.delete("/:id", articleController.article_delete);
 
 module.exports = router;
