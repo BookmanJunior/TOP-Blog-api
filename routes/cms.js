350575c64eb69ddab797ca85adfa8cef5dbe9e60
Azam Gulov <gulovazam@gmail.com>
2024-03-25 20:04:00 +0500

    Add new routes

--- routes/cms.js
+++ routes/cms.js
@@ -2,11 +2,44 @@ const router = require("express").Router();
 const loginController = require("../controllers/loginController");
 const authorizationController = require("../controllers/authorizationController");
 const userController = require("../controllers/userController");
+const cmsController = require("../controllers/cmsController");
+const Role = require("../models/Role");
 
 router.post("/login", loginController.login_cms);
 
-router.get("/users", userController.users_get);
+router.delete("/articles/:id", articleController.article_delete);
 
-router.get("/users/:id", userController.user_get);
+router.put("/articles/:id", articleController.article_edit);
+
+router.put("/articles/:id/checkbox", articleController.article_checkbox_update);
+
+router.get("/users", authorizationController.isAdmin, userController.users_get);
+
+router.get(
+  "/users/:id",
+  authorizationController.isAdmin,
+  userController.user_get
+);
+
+router.put(
+  "/users/:id",
+  authorizationController.isAdmin,
+  userController.user_update
+);
+
+router.get(
+  "/roles",
+  authorizationController.isAdmin,
+  async (req, res, next) => {
+    try {
+      const roles = await Role.find({}).exec();
+      return res.status(200).send(roles);
+    } catch (error) {
+      return next(error);
+    }
+  }
+);
+
+router.get("/count", cmsController.documents_count_get);
 
 module.exports = router;
