ce41870555bf2c91d878811e61f7a9d64d051a87
Azam Gulov <gulovazam@gmail.com>
2024-04-02 17:30:27 +0500

    initial commit

---
+++ validators/CustomValidator.js
@@ -0,0 +1,31 @@
+const { ExpressValidator } = require("express-validator");
+const User = require("../models/User");
+const Role = require("../models/Role");
+
+const CustomValidator = new ExpressValidator({
+  isUsernameTaken: async (value, { req }) => {
+    const user = await User.findOne({ username: value })
+      .collation({ locale: "en", strength: 2 })
+      .exec();
+
+    if (user && user.id !== req.body._id) {
+      throw new Error(
+        `${value} already exists. Please pick a different username`
+      );
+    }
+  },
+  isPasswordMatch: async (value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("Passwords don't match");
+    }
+  },
+  isRole: async (value) => {
+    const role = Role.findOne({ role: value }).exec();
+
+    if (!role) {
+      throw new Error("Please pick an existing role");
+    }
+  },
+});
+
+module.exports = CustomValidator;
---
+++ validators/UserValidation.js
@@ -0,0 +1,21 @@
+const { body } = require("./CustomValidator");
+
+exports.UserValidation = [
+  body("username", "Username must be at least 4 characters long")
+    .trim()
+    .isLength({ min: 4 })
+    .bail()
+    .isUsernameTaken()
+    .escape(),
+  body("password", "Password must be at least 8 characters long")
+    .trim()
+    .isLength({ min: 8 })
+    .escape(),
+  body("confirmPassword", "Confirm Password must be at least 8 characters long")
+    .trim()
+    .isPasswordMatch()
+    .bail()
+    .isLength({ min: 8 })
+    .escape(),
+  body("name.*").optional({ checkFalsy: true }).trim().escape(),
+];
