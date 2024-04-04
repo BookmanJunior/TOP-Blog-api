2daa09b192a6d9a1f08bd96b0a218d73a1e49f3b
Azam Gulov <gulovazam@gmail.com>
2024-04-04 18:18:59 +0500

    Add middleware to post route

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const loginController = require("../controllers/loginController");
+const userController = require("../controllers/userController");
 
-router.post("/", loginController.login);
+router.post("/", loginController.login, userController.me);
 
 module.exports = router;
