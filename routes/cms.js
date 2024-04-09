f6d92743130d7bf026189c9f412fffd535989bb5
Azam Gulov <gulovazam@gmail.com>
2024-04-09 17:07:23 +0500

    Remove authorizationController middleware from routes
    remove login middleware
    remove unused imports

--- routes/cms.js
+++ routes/cms.js
@@ -1,13 +1,8 @@
 const router = require("express").Router();
-const loginController = require("../controllers/loginController");
-const authorizationController = require("../controllers/authorizationController");
 const userController = require("../controllers/userController");
 const cmsController = require("../controllers/cmsController");
 const articleController = require("../controllers/articleController");
-const Role = require("../models/Role");
 const { UserValidation } = require("../validators/UserValidation");
-
-router.post("/login", loginController.login_cms, userController.me);
 
 router.delete("/articles/:id", articleController.article_delete);
 
@@ -15,25 +10,17 @@ router.put("/articles/:id", articleController.article_edit);
 
 router.put("/articles/:id/checkbox", articleController.article_checkbox_update);
 
-router.get("/users", authorizationController.isAdmin, userController.users_get);
+router.get("/users", userController.users_get);
 
 router.delete("/users/:id", userController.user_delete);
 
-router.get(
-  "/users/:id",
-  authorizationController.isAdmin,
-  userController.user_get
-);
+router.get("/users/:id", userController.user_get);
 
-router.put(
-  "/users/:id",
-  authorizationController.isAdmin,
-  userController.user_update
-);
+router.put("/users/:id", userController.user_update);
 
 router.post("/users", UserValidation, userController.user_cms_post);
 
-router.get("/roles", authorizationController.isAdmin, cmsController.roles_get);
+router.get("/roles", cmsController.roles_get);
 
 router.get("/count", cmsController.documents_count_get);
 
