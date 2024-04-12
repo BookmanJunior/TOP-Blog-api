446d9f33e3c7fe6299617b67c7d098ab38905d1f
Azam Gulov <gulovazam@gmail.com>
2024-04-12 20:09:23 +0500

    Add authorization middleware to delete route

--- routes/comment.js
+++ routes/comment.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const commentController = require("../controllers/commentController");
+const authorizationController = require("../controllers/authorizationController");
 const router = express.Router();
 
 router.post("/", commentController.comment_post);
 
-router.delete("/:id", commentController.comment_delete);
+router.delete(
+  "/:id",
+  authorizationController.isAuthorized,
+  commentController.comment_delete
+);
 
 module.exports = router;
