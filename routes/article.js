f787275149d803ec4c6e7accd0a12893ba55958e
Azam Gulov <gulovazam@gmail.com>
2024-04-13 19:48:32 +0500

    Replace root get request middleware
    remove post request

--- routes/article.js
+++ routes/article.js
@@ -1,15 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const articleController = require("../controllers/articleController");
-const authorizationController = require("../controllers/authorizationController");
 
-router.get("/", articleController.articles_get);
-
-router.post(
-  "/",
-  authorizationController.isAdmin,
-  articleController.article_post
-);
+router.get("/", articleController.published_articles_get);
 
 router.get("/:id", articleController.article_get);
 
