ffaba7887578bfa2f498fcbb32824ea05a08bbcb
Azam Gulov <gulovazam@gmail.com>
2024-04-15 21:07:27 +0500

    Add login and me middleware to post route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const { UserValidation } = require("../validators/UserValidation");
+const loginController = require("../controllers/loginController");
 
-router.post("/", UserValidation(), userController.user_post);
+router.post(
+  "/",
+  UserValidation(),
+  userController.user_post,
+  loginController.login,
+  userController.me
+);
 
 module.exports = router;
