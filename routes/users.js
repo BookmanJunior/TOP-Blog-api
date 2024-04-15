1492031bf5cd91313463abebbae4d5faa7b70412
Azam Gulov <gulovazam@gmail.com>
2024-04-15 20:29:51 +0500

    Call UserValidation inside post route

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const { UserValidation } = require("../validators/UserValidation");
 
-router.post("/", UserValidation, userController.user_post);
+router.post("/", UserValidation(), userController.user_post);
 
 module.exports = router;
