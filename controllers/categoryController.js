fb6d0d63fbfb9ca5a1c7a6e6dc656ae25d505711
Azam Gulov <gulovazam@gmail.com>
2024-04-21 22:50:53 +0500

    Add categories_edit middleware
    fix isEmpty method not being invoked in categories_post middleware

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const Category = require("../models/Category");
 const { body, validationResult } = require("../validators/CustomValidator");
 const { ErrorFormatter } = require("../helpers/ErrorFormatter");
+const { isValidObjectId } = require("mongoose");
 
 exports.categories_get = async (req, res, next) => {
   try {
@@ -8,6 +9,19 @@ exports.categories_get = async (req, res, next) => {
     return res.status(200).send(categories);
   } catch (error) {
     next(error);
+  }
+};
+
+exports.category_get = async (req, res, next) => {
+  if (!isValidObjectId(req.params.id)) {
+    return res.status(404).send({ message: "Category not found" });
+  }
+
+  try {
+    const category = await Category.findById(req.params.id).exec();
+    return res.status(200).send(category);
+  } catch (error) {
+    return next(error);
   }
 };
 
@@ -23,7 +37,32 @@ exports.categories_post = [
     const newCategory = new Category({ title: req.body.title });
     const errors = validationResult(req).formatWith(ErrorFormatter);
 
-    if (!errors.isEmpty) {
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.mapped());
+    }
+
+    try {
+      await newCategory.save();
+      return res.status(200).send(newCategory);
+    } catch (error) {
+      next(error);
+    }
+  },
+];
+
+exports.categories_edit = [
+  body("title", "Category must be at least 3 characters long")
+    .trim()
+    .isLength({ min: 3 })
+    .bail()
+    .isCategory()
+    .escape(),
+
+  async (req, res, next) => {
+    const newCategory = new Category({ title: req.body.title });
+    const errors = validationResult(req).formatWith(ErrorFormatter);
+
+    if (!errors.isEmpty()) {
       return res.status(400).send(errors.mapped());
     }
 
