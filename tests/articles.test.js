b526afc3876efc2cb5dc389076396e6f22e686e6
Azam Gulov <gulovazam@gmail.com>
2024-04-29 19:09:50 +0500

    Add test suite
    add a test and pass it
    remove date property from mock article
    add get route

--- tests/articles.test.js
+++ tests/articles.test.js
@@ -11,7 +11,10 @@ app.use((req, res, next) => {
   res.locals.currentUser = "65cde24a86437d8bd4d493a4";
   next();
 });
+
+app.get("/article/:id", articleController.article_get);
 app.post("/article", ArticleValidation(), articleController.article_post);
+app.put("/article/:id", ArticleValidation(), articleController.article_edit);
 
 describe("Article post tests", () => {
   const mockArticle = {
@@ -22,7 +25,6 @@ describe("Article post tests", () => {
     category: "6621205cbc0ae006099a1d05",
     featured: false,
     published: true,
-    date: Date.now,
   };
 
   beforeAll(MongoMemory.initializeMongoServer);
@@ -38,5 +40,39 @@ describe("Article post tests", () => {
 
   it("New article should fail validation", (done) => {
     request(app).post("/article").type("form").send({}).expect(422, done);
+  });
+});
+
+describe("Article edit test", () => {
+  const mockArticle = {
+    title: "Test article",
+    content: "The content goes here",
+    cover: "https://media.graphassets.com/fbC623WSCiSdF9djpWQo",
+    comments: [],
+    category: "6621205cbc0ae006099a1d05",
+    featured: false,
+    published: true,
+  };
+
+  const mockArticleChanged = {
+    ...mockArticle,
+    title: "New title",
+  };
+
+  beforeAll(MongoMemory.initializeMongoServer);
+  afterAll(MongoMemory.closeMongoServer);
+
+  it("Create new article and change the title", async () => {
+    const data = await request(app)
+      .post("/article")
+      .type("form")
+      .send(mockArticle);
+    const createdArticleId = data._body._id;
+    mockArticleChanged._id = createdArticleId;
+    const response = await request(app)
+      .put(`/article/${createdArticleId}`)
+      .type("form")
+      .send(mockArticleChanged);
+    expect(response._body).toHaveProperty("title", mockArticleChanged.title);
   });
 });
