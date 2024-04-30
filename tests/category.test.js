a28fd6fc496290733bb34508956a47926ea2f0e9
Azam Gulov <gulovazam@gmail.com>
2024-04-30 17:39:38 +0500

    Add category post route test suite

---
+++ tests/category.test.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const request = require("supertest");
+const MongoServer = require("./mongoConfigTesting");
+const categoryController = require("../controllers/categoryController");
+
+app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+  res.locals.currentUser = "65cde24a86437d8bd4d493a4";
+  next();
+});
+
+app.post("/category", categoryController.categories_post);
+
+describe("Category post route tests", () => {
+  beforeAll(MongoServer.initializeMongoServer);
+  afterAll(MongoServer.closeMongoServer);
+
+  it("Successfully create a category", async () => {
+    const res = await request(app)
+      .post("/category")
+      .type("form")
+      .send({ title: "test" });
+    expect(res._body).toHaveProperty("title", "test");
+  });
+
+  it("Prevent creating an empty category", async () => {
+    await request(app).post("/category").type("form").send({});
+    expect(400);
+  });
+
+  it("Prevent creating a duplicate category", async () => {
+    const res = await request(app)
+      .post("/category")
+      .type("form")
+      .send({ title: "test" });
+    const duplicateRes = await request(app)
+      .post("/category")
+      .type("form")
+      .send({ title: "Test" });
+    expect(400);
+  });
+});
