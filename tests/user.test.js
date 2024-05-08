b4a08f0208175d3e7062004cb86f5dff96b5560a
Azam Gulov <gulovazam@gmail.com>
2024-05-08 12:25:31 +0500

    Add tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -16,3 +16,27 @@ app.get("/user/:id", userController.user_get);
 app.post("/users", UserValidation(), userController.user_post);
 app.put("/users/:id", UserValidation(), userController.user_update);
 app.delete("/users/:id", userController.user_delete);
+
+describe("Create new user", () => {
+  beforeAll(MongoServer.initializeMongoServer);
+  afterAll(MongoServer.closeMongoServer);
+
+  const mockUser = {
+    username: "john",
+    password: "ABCDE12345*#",
+    confirmPassword: "ABCDE12345*#",
+  };
+
+  it("Creates user successfully", (done) => {
+    request(app).post("/users").type("form").send(mockUser).expect(200, done);
+  });
+
+  it("User fails validation", (done) => {
+    request(app).post("/users").type("form").send({}).expect(422, done);
+  });
+
+  it("Prevents creating username with duplicate username", async () => {
+    await request(app).post("/users").type("form").send(mockUser);
+    await request(app).post("/users").type("form").send({}).expect(422);
+  });
+});
